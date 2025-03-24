f7aab93e4b4aca107e7210ba0f06e4e497dc9701
nicacevedo <ni.acevedo.villena@gmail.com>
2025-03-24 03:29:50 -0400

    [Update]

--- image_conversor.py
+++ image_conversor.py
@@ -21,8 +21,22 @@ def convert_heic_to_jpg(input_path, output_path, new_size=None):
     print(f"Converted and saved: {output_path}")
 
 
+def convert_jpg_to_png(input_path, output_path, new_size=None):
+    # Open JPG image
+    image = Image.open(input_path)
+    
+    # Resize the image if new_size is provided
+    if new_size:
+        image = image.resize(new_size, Image.LANCZOS)
+    
+    # Save as JPG
+    image.save(output_path, "PNG")
+    print(f"Converted and saved: {output_path}")
+
 # Example usage
-input_file = "./images/profile.heic"
-output_file = "images/profile_new.jpg"
-new_dimensions = (720, 960)  # Set new dimensions (width, height)
-convert_heic_to_jpg(input_file, output_file, new_dimensions)
+# input_file = "./images/profile.heic"
+input_file = "./images/profile_new.jpg"
+output_file = "images/profile_new.png"
+new_dimensions = (720, 720)  # Set new dimensions (width, height)
+# convert_heic_to_jpg(input_file, output_file, new_dimensions)
+convert_jpg_to_png(input_file, output_file, new_size=new_dimensions)
